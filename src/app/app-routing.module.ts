f4165edf726f254cf545b1a55863faef5dfcee7f
gj1153 <pramodpr316@users.noreply.github.com>
2021-01-11 19:03:49 -0500

    Initial Commit - Scaffolding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "lms/home",
+    pathMatch: "full",
+  },
+  {
+    path: "lms/home",
+    component: AppComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
